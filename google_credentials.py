560a4983e50b9980dedfd2c9cc7632aaa5d098f2
Urzhumtsev <urzhumtsev.max@gmail.com>
2018-10-12 16:17:37 +0300

    key update

--- google_credentials.py
+++ google_credentials.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
 import httplib2
 import apiclient
 from oauth2client.service_account import ServiceAccountCredentials
@@ -5,7 +8,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 class GgCredits:
     def __init__(self):
-        self.CREDENTIALS_FILE = "/var/www/telegrambots/kpi/table-b0cc9ceed3d8.json"  # имя файла с закрытым ключом
+        self.CREDENTIALS_FILE = "/var/www/telegrambots/kpi/table-b0cc9ceed3d8.json" 
         self.credentials = ServiceAccountCredentials.from_json_keyfile_name(self.CREDENTIALS_FILE,
                                                                             ['https://www.googleapis.com/auth/spreadsheets',
                                                                              'https://www.googleapis.com/auth/drive'])
