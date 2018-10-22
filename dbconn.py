c30bc752b1425b4f5d2e4335bc23ab7f8374ced2
Urzhumtsev <urzhumtsev.max@gmail.com>
2018-10-22 15:06:46 +0300

    Schema added

--- dbconn.py
+++ dbconn.py
@@ -4,8 +4,8 @@
 import psycopg2
 import constants
 
-
-class PGadmin:
+# Please change 'botname.' to your schema
+class PgAdmin:
     def __init__(self):
         self.connection = psycopg2.connect(database=constants.DATABASE,
                                            user=constants.USER,
@@ -14,20 +14,8 @@ class PGadmin:
                                            port=constants.PORT)
         self.cursor = self.connection.cursor()
 
-    def select_all(self, table):
-        self.cursor.execute('''SELECT * FROM {}'''.format(table))
-        return self.cursor.fetchall()
-
     def select_all1(self, column, table, parameter):
-        self.cursor.execute('''SELECT {0} FROM {1} WHERE {2}'''.format(column, table, parameter))
-        return self.cursor.fetchall()
-
-    def select_all2(self, column, table):
-        self.cursor.execute('''SELECT {0} FROM {1}'''.format(column, table))
-        return self.cursor.fetchall()
-
-    def select_all3(self, parameter):
-        self.cursor.execute('''SELECT {}'''.format(parameter))
+        self.cursor.execute('''SELECT {0} FROM botname.{1} WHERE {2}'''.format(column, table, parameter))
         return self.cursor.fetchall()
 
     def select(self, parameter):
@@ -35,23 +23,19 @@ class PGadmin:
         return self.cursor.fetchone()
 
     def select_single(self, table, column, parameter):
-        self.cursor.execute('''SELECT {0} FROM {1} WHERE {2} '''.format(table, column, parameter))
+        self.cursor.execute('''SELECT {0} FROM botname.{1} WHERE {2} '''.format(table, column, parameter))
         return self.cursor.fetchone()
 
     def delete_single(self, table, column, parameter):
-        self.cursor.execute('''DELETE FROM {0} WHERE {1} = {2}'''.format(table, column, parameter))
-        return self.connection.commit()
-
-    def delete(self, table, parameter):
-        self.cursor.execute('''DELETE FROM {0} WHERE {1}'''.format(table, parameter))
+        self.cursor.execute('''DELETE FROM botname.{0} WHERE {1} = {2}'''.format(table, column, parameter))
         return self.connection.commit()
 
     def insert(self, tablecolumns, values):
-        self.cursor.execute("""INSERT INTO {0} VALUES {1};""".format(tablecolumns, values))
+        self.cursor.execute("""INSERT INTO botname.{0} VALUES {1};""".format(tablecolumns, values))
         return self.connection.commit()
 
     def update(self, table, values, condition):
-        self.cursor.execute("""UPDATE {0} SET {1} WHERE {2}""".format(table, values, condition))
+        self.cursor.execute("""UPDATE botname.{0} SET {1} WHERE {2}""".format(table, values, condition))
         return self.connection.commit()
 
     def close(self):
